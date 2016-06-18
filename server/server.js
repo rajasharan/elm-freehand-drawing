6c5ca2202026f1fa76779fcf1411bcd21237aa0b
Raja Sharan Mamidala <rajasharan@gmail.com>
2016-06-18 13:10:19 -0400

    remove express from server dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 3000 });
+console.log('WebSocketServer started on port 3000');
 
 wss.on('connection', function connection(sender) {
   sender.send('connected');
 
   sender.on('message', function incoming(message) {
     var msg = message.split('(').join('[').split(')').join(']');
-    console.log('received: %s', msg);
+    //console.log('received: %s', msg);
 
     wss.clients.forEach(function each(client) {
         if (client !== sender) {
